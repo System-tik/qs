be1a9a609b5a49cce728f369315f31d3dad9b5b3
elshammar <kanyindamampova@gmail.com>
2022-08-13 19:04:01 +0200

    joel commit 1

---
+++ app/Models/categorieprod.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class categorieprod extends Model
+{
+    use HasFactory;
+}
