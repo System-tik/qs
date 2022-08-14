3da4f7392d1e4f8ce73424ff8ac319856b086308
elshammar <kanyindamampova@gmail.com>
2022-08-15 00:12:21 +0200

    joel 3 admin/cat et pro

--- app/Http/Livewire/Admin/VCatproduit.php
+++ app/Http/Livewire/Admin/VCatproduit.php
@@ -3,11 +3,58 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\categorieprod;
+
 
 class VCatproduit extends Component
 {
+    public $nom;
+    public $selectedId;
+    public $categories;
+
     public function render()
     {
+        $this->categories=categorieprod::all();
         return view('livewire.admin.v-catproduit');
+    }
+    public function add(){
+        $validate=$this->validate([
+            'nom'=>'required',   
+        ]);
+
+        categorieprod::create($validate);
+        $this->clear();
+
+    }
+    public function clear()
+    {
+        $this->nom="";
+    }
+
+    public function update()
+    {
+        $valider = $this->validate([
+            'nom'=>'required'
+        ]);
+        $valide = categorieprod::find($this->selectedId);
+        $valide->update($valider);
+        $this->clear();
+    }
+
+
+    public function recu($data)
+    {
+        $this->selectedId=$data['id'];
+        $this->nom=$data['nom'];
+    }
+
+    public function delete()
+    {
+        $valider = $this->validate([
+            'nom'=>'required'
+        ]);
+        $record = categorieprod::find($this->selectedId);
+        $record->delete();
+        $this->clear();
     }
 }
--- app/Http/Livewire/Admin/VProduit.php
+++ app/Http/Livewire/Admin/VProduit.php
@@ -3,11 +3,86 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\produit;
 
 class VProduit extends Component
 {
+    public $id_catp=1;
+    public $nom;
+    public $prix;
+    public $descrip;
+    public $type;
+    public $poids;
+    public $selectId;
+    public $produits;
+
     public function render()
     {
+        $this->produits=produit::all();
+
         return view('livewire.admin.v-produit');
     }
+
+    public function add(){
+        $validate=$this->validate([
+            'id_catp'=>'required',
+            'nom'=>'required',
+            'prix'=>'required',
+            'descrip'=>'required',
+            'type'=>'required',
+            'poids'=>'required'
+            
+        ]);
+
+        produit::create($validate);
+    }
+
+    public function clear()
+    {
+        $this->id_catp="";
+        $this->nom="";
+        $this->prix="";
+        $this->descrip="";
+        $this->type="";
+        $this->poids="";
+    }
+
+    public function update()
+    {
+        $valider = $this->validate([
+            'id_catp'=>'required',
+            'nom'=>'required',
+            'prix'=>'required',
+            'descrip'=>'required',
+            'type'=>'required',
+            'poids'=>'required'
+        ]);
+        $valide = produit::find($this->selectedId);
+        $valide->update($valider);
+        $this->clear();
+    }
+
+
+    public function recu($data)
+    {
+        $this->selectedId=$data['id'];
+        $this->nom=$data['nom'];
+    }
+
+    public function delete()
+    {
+        $valider = $this->validate([
+            'id_catp'=>'required',
+            'nom'=>'required',
+            'prix'=>'required',
+            'descrip'=>'required',
+            'type'=>'required',
+            'poids'=>'required'
+        ]);
+        $record = produit::find($this->selectedId);
+        $record->delete();
+        $this->clear();
+    }
+
+
 }
